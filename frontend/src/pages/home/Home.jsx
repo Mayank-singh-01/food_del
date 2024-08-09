c443250081d89564811a0b2ec58cd93e5f3dbe4f
Mayank Singh <mayank23378@gmail.com>
2024-08-10 01:23:23 +0530

    explore section done

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Home.css";
 import Header from '../../components/header/Header';
 import ExploreMenu from '../../components/exploreMenu/ExploreMenu';
 
 function Home() {
+
+  const [category,setCategory] = useState("All")
+
   return (
     <div>
       <Header/>
-      <ExploreMenu/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
     </div>
   )
 }
 
-export default Home
+export default Home 
