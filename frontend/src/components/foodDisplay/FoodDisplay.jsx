1ab322c223fc19e5ac5f7432c094a97c75470cbe
Mayank Singh <mayank23378@gmail.com>
2024-08-10 20:26:14 +0530

    resolve error and display items using context

--- frontend/src/components/foodDisplay/FoodDisplay.jsx
+++ frontend/src/components/foodDisplay/FoodDisplay.jsx
@@ -12,7 +12,7 @@ function FoodDisplay({category}) {
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
          {food_list.map((item,index)=>{
-            return<FoodItem key={index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image} />
+            return<FoodItem key={index} id={index} name={item.name} description={item.description} price={item.price} image={item.image} />
          })}
       </div>
     </div>
--- frontend/src/components/foodItem/FoodItem.jsx
+++ frontend/src/components/foodItem/FoodItem.jsx
@@ -1,36 +1,38 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import "./FoodItem.css";
 import { assets } from "../../assets/assets";
+import { StoreContext } from "../../context/StoreContext";
 
 function FoodItem({ id, name, price, description, image }) {
 
-  const [itemCount, setItemCount] = useState(0);
+  const {cartItems,addToCart,removeFromCart} = useContext(StoreContext)
 
   return (
     <div className="food-item">
       <div className="food-item-img-container">
         <img className="food-item-image" src={image} alt="" />
-        {!itemCount 
-        ?<img
-        className="add"
-            onClick={() => setItemCount((prev) => prev + 1)}
+        {!cartItems[id] ? (
+          <img
+            className="add"
+            onClick={() => addToCart(id)}
             src={assets.add_icon_white}
             alt=""
           />
-          :<div className="food-item-count">
+        ) : (
+          <div className="food-item-count">
             <img
-              onClick={() => setItemCount((prev) => prev - 1)}
+              onClick={() => removeFromCart(id)}
               src={assets.remove_icon_red}
               alt=""
             />
-            <p>{itemCount}</p>
+            <p>{cartItems[id]}</p>
             <img
-              onClick={() => setItemCount((prev) => prev + 1)}
+              onClick={() => addToCart(id)}
               src={assets.add_icon_green}
               alt=""
             />
           </div>
-        }
+        )}
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
