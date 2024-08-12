881695457deda28fdb344d10963869c7ee60b7f4
Mayank Singh <mayank23378@gmail.com>
2024-08-12 18:22:14 +0530

    done with add to cart section

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,19 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import "./Navbar.css"
 import {assets} from "../../assets/assets"
 import {Link} from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext';
 
 function navbar({ setShowLogin }) {
   const [menu, setMenu] = useState("home");
 
+  const{getTotalCartAmount} = useContext(StoreContext);
+
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="" className="logo" />
+      <Link to="/">
+        <img src={assets.logo} alt="" className="logo" />
+      </Link>
       <ul className="navbar-menu">
         <Link
           to={"/"}
@@ -52,10 +57,12 @@ function navbar({ setShowLogin }) {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
-          <div className="dot"></div>
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="" />
+          </Link>
+          <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
-        <button onClick={()=>setShowLogin(true)} >sing in</button>
+        <button onClick={() => setShowLogin(true)}>sing in</button>
       </div>
     </div>
   );
