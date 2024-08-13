0d52e2998df584d6f060e50cdb5b54d2e76249a9
Mayank Singh <mayank23378@gmail.com>
2024-08-13 21:47:29 +0530

    database setup

---
+++ backend/controllers/foodControllers.js
@@ -0,0 +1,11 @@
+import foodModel from "../models/foodModels.js";
+import fs from 'fs';
+
+
+//add food items
+
+const addFood = async (req,res) =>{
+
+}
+
+export {addFood}
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import { connectDB } from "./config/db.js";
+import fodRouter from "./routes/foodRoute.js";
 
 
 //app config
@@ -9,6 +11,12 @@ const port = 4000
 //middleware
 app.use(express.json())
 app.use(cors())
+
+//db connection
+connectDB();
+
+//api endpoints
+app.use("/api/food",fodRouter)
 
 app.get("/",(req,res)=>{
    res.send("hay, API is working")
