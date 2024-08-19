e85d4b60178f738203bebd60958778fd937af8a5
Mayank Singh <mayank23378@gmail.com>
2024-08-19 13:16:46 +0530

    done with register user

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,63 @@
+import userModel from "../models/UserModel.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import validator from "validator";
+
+
+//login user 
+const loginUser = async (req,res) => {
+   const {email,password} = req.body;
+   try {
+      const user = await userModel.findOne({email});
+   } catch (error) {
+      
+   }
+}
+
+//creat token 
+const createToken = (id) => {
+   return jwt.sign({ id }, process.env.JWT_SECRET);
+}
+
+// regidter user 
+const registerUser = async (req,res) => {
+   const {name,password,email} = req.body;
+   try {
+
+      //check for the existing user 
+      const exist = await userModel.findOne({email});
+      if(exist) {
+         return res.json({success:false,massage:"user already exist"})
+      }
+
+      //validating email and strong password 
+      if(!validator.isEmail(email)) {
+         return res.json({ success: false, massage: "Please enter a valid email"});
+      }
+      if(password.length<8) {
+         return res.json({success: false,massage: "Password showld be grater then 8 degit",});
+      }
+
+      //hassing user password 
+      const salt = await bcrypt.genSalt(10)
+      const hashedPassword = await bcrypt.hash(password,salt);
+
+      const newUser = new userModel({
+         name:name,
+         email:email,
+         password:hashedPassword
+      })
+
+      const user = await newUser.save();
+      const token = createToken(user._id);
+      res.json({success:true,token})
+
+   } catch (error) {
+      console.log(error);
+      res.json({success:false,massage:"Error while registered user"})
+      
+   }
+}
+
+
+export {loginUser,registerUser}
--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,33 @@ import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import fodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/UserRoute.js";
+import 'dotenv/config'
 
 
 //app config
 const app = express()
 const port = 4000
 
+
 //middleware
 app.use(express.json())
 app.use(cors())
 
+
 //db connection
 connectDB();
+
 
 //api endpoints
 app.use("/api/food",fodRouter)
 app.use("/image",express.static('uploads'))
+app.use("/api/user",userRouter)
+
 
 app.get("/",(req,res)=>{
    res.send("hay, API is working")
+
 })
 
 app.listen(port,()=>{
