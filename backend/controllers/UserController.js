6bbd207651c37beca14b22b063b4484d7209c231
Mayank Singh <mayank23378@gmail.com>
2024-08-22 20:43:55 +0530

    embed logic of login and register user on backend

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -9,7 +9,23 @@ const loginUser = async (req,res) => {
    const {email,password} = req.body;
    try {
       const user = await userModel.findOne({email});
+
+      if (!user) {
+         return res.json({success:false,massage:"User Dosn't exist with this emial"});
+      }
+
+      const isMatch = await bcrypt.compare(password,user.password);
+
+      if (!isMatch) {
+         return res.json({success:false,massage:"Invalid Password"});
+      }
+
+      const token = createToken(user._id);
+      res.json({success:true,token})
+
    } catch (error) {
+      console.log(error);
+      res.json({success:false,massage:"Error while login"})
       
    }
 }
